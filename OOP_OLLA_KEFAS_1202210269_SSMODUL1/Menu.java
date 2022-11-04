6b53ce13de6e764c38c6ec7971997ccb24830120
KefasPakpahan <108292039+KefasPakpahan@users.noreply.github.com>
2022-11-04 16:49:41 +0700

    Add files via upload

---
+++ OOP_OLLA_KEFAS_1202210269_SSMODUL1/Menu.java
@@ -0,0 +1,20 @@
+import java.util.jar.Attributes.Name;
+
+public class Menu {
+
+    String Makanan;
+    String Minuman;
+    String Dessert;
+
+    public void setMakanan(String makanan){
+        this.makanan = Makanan;
+    }
+    public void setMinuman(String minuman){
+        this.minuman = Minuman;
+    }
+    public void setDessert(String dessert){
+        this.dessert = Dessert;
+    }
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
+
+}
---
+++ OOP_OLLA_KEFAS_1202210269_SSMODUL1/User.java
@@ -0,0 +1,16 @@
+public class User {
+    String Nama;
+    String NomorTelp;
+
+    public void setnama(String nama){
+        this.nama = Nama;
+    }
+    public void setnomortelp(String nomortelp){
+        this.nomortelp = NomorTelp;
+    }
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+
+
+}
